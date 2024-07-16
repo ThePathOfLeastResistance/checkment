e6b08658f810715952fe161170646c378988eec9
Jamerson <jamescao800@gmail.com>
2024-07-16 14:29:51 -0400

    it works !!!!

--- background/messages/ping.ts
+++ background/messages/ping.ts
@@ -1,11 +1,11 @@
 import type { PlasmoMessaging } from "@plasmohq/messaging"
 
-
 console.log("hello from the backend")
 
 const handler: PlasmoMessaging.MessageHandler = async (req, res) => {
   const message = await req.body.id
-
+  console.log("sent the message")
+  console.log(message)
   res.send({
     message
   })
