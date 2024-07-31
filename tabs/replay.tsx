63d5562d5d40f4f14f3b9c250f7a5e399aab49cc
Jamerson <jamescao800@gmail.com>
2024-07-31 19:11:17 -0400

    Made changes

--- tabs/replay.tsx
+++ tabs/replay.tsx
@@ -1,22 +1,54 @@
+import styleText from "data-text:./replay.module.css"
+import type { PlasmoCSConfig, PlasmoGetStyle } from "plasmo"
+import { useEffect, useState } from "react"
+
+import * as style from "./replay.module.css"
+
+export const getStyle = () => {
+  const style = document.createElement("style")
+  style.textContent = styleText
+  return style
+}
+
 export default function DeltaFlyerPage() {
-  window.addEventListener("message", (event) => {
-    if (event.source !== window && event.data.type === "MY_DATA") {
-      const data = event.data.payload
-      console.log("Received data:", data)
-      // Handle your data here
-    }
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Background script received a message:", request)
+    sendResponse({ message: "Background script has received the message" })
   })
+
+  let [message, setMessage] = useState()
+  let [data, setData] = useState()
+  let [flags, setFlags] = useState()
+
   console.log("Background script is running")
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>Delta Flyer Tab</h2>
-
-      <p>This tab is only available on the Delta Flyer page.</p>
+    <div className={style.bodyStyle}>
+      <h3>Tittle:</h3>
+      <h2>{}</h2>
+      <div>
+        <div>
+          <h3>Editors:</h3>
+          <h2>{}</h2>
+        </div>
+        <div>
+          <h3>Plaing Speed:</h3>
+          <h2>{}</h2>
+        </div>
+      </div>
+      <div>
+        <button>Play</button>
+        <h1>scroll</h1>
+      </div>
+      <div>
+        <div>
+          <h3>Time:</h3>
+          <h2>{}</h2>
+        </div>
+        <div>
+          <h3>Flags:</h3>
+          <h2>{}</h2>
+        </div>
+      </div>
     </div>
   )
 }
