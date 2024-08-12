b0b77c86d62743b0ac87578368c6bca46b036c1c
Jamerson <jamescao800@gmail.com>
2024-08-12 15:19:58 -0400

    added the classes - James

--- tabs/replay.tsx
+++ tabs/replay.tsx
@@ -1,85 +1,55 @@
-import { useReducer } from "react";
+import type { PlasmoCSConfig, PlasmoGetStyle } from "plasmo";
+import { useEffect, useRef, useState } from "react";
 
 import "../style.css";
 
-function IndexPopup() {
-  const [count, increase] = useReducer((c) => c + 1, 0);
+const DeltaFlyerPage = () => {
+  const [message, setMessage] = useState();
+  const [data, setData] = useState();
+  const [flags, setFlags] = useState();
+  const [map, setmap] = useState();
+  const [sliderRange, setSliderRange] = useState();
+  const [inputValue, setInputValue] = useState();
 
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    console.log("Background script received a message:", request);
+  });
+  console.log("Background script is running");
   return (
-    <button
-      onClick={() => increase()}
-      type="button"
-      className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-    >
-      Count:
-      <span className="inline-flex items-center justify-center w-8 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
-        {count}
-      </span>
-    </button>
+    <div className="px-32 py-4 bg-[#F1F6F9] h-screen	w-screen">
+      <h3 className="text-base text-black">Tittle:</h3>
+      <h2 className="text-xl text-black">{}</h2>
+      <div className="flex">
+        <div className="inline-block h-8 my-4">
+          <h3>Editors:</h3>
+          <h2>{}</h2>
+        </div>
+        <div className="inline-block h-8 mx-4">
+          <h3>Plaing Speed:</h3>
+          <h2>{}</h2>
+        </div>
+      </div>
+      <div className="flex">
+        <div className="inline-block h-4 mx-4">
+          <button className="px-2 py-3 outline outline-2 ">Play</button>
+        </div>
+        <div className="inline-block h-4 mx-4">
+          <h3>Plaing Speed:</h3>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <h3>Time:</h3>
+          <h2>{}</h2>
+        </div>
+        <div>
+          <h3>Flags:</h3>
+          <h2>{}</h2>
+        </div>
+      </div>
+    </div>
   );
-}
+};
 
-export default IndexPopup;
-
-// import type { PlasmoCSConfig, PlasmoGetStyle } from "plasmo";
-// import { useEffect, useRef, useState } from "react";
-
-// import cssText from "data-text:~style.css";
-
-// export const getStyle = () => {
-//   const style = document.createElement("style");
-//   style.textContent = cssText;
-//   return style;
-// };
-
-// const DeltaFlyerPage = () => {
-//   const [message, setMessage] = useState();
-//   const [data, setData] = useState();
-//   const [flags, setFlags] = useState();
-//   const [map, setmap] = useState();
-//   const [sliderRange, setSliderRange] = useState();
-//   const [inputValue, setInputValue] = useState();
-
-//   chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//     console.log("Background script received a message:", request);
-//   });
-//   console.log("Background script is running");
-//   return (
-//     <div className="px-20">
-//       <button
-//         type="button"
-//         className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-//       >
-//         Count:
-//         <span className="inline-flex items-center justify-center w-8 h-4 ml-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full"></span>
-//       </button>
-//       <h3 className="text-white">Tittle:</h3>
-//       <h2>{}</h2>
-//       <div>
-//         <div>
-//           <h3>Editors:</h3>
-//           <h2>{}</h2>
-//         </div>
-//         <div>
-//           <h3>Plaing Speed:</h3>
-//           <h2>{}</h2>
-//         </div>
-//       </div>
-//       <div>
-//         <button> Play</button>
-//       </div>
-//       <div>
-//         <div>
-//           <h3>Time:</h3>
-//           <h2>{}</h2>
-//         </div>
-//         <div>
-//           <h3>Flags:</h3>
-//           <h2>{}</h2>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DeltaFlyerPage;
+export default DeltaFlyerPage;
